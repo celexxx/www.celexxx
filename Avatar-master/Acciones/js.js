aa3ce5f554ca1d6c9373d2bde13c46724b563736
celexxx <152182791+celexxx@users.noreply.github.com>
2023-11-27 10:46:22 -0300

    Add files via upload

---
+++ Avatar-master/Acciones/js.js
@@ -0,0 +1,7 @@
+const Menu = document.getElementById("menu_icono");
+const cerrar_ventana= document.getElementById("menu_url");
+
+Menu.addEventListener('Click',()=>{
+
+    cerrar_ventana.classList.toggle("Rocio");
+})
